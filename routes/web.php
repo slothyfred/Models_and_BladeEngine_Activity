06b3858c22075ed1b087d20381380641134af30b
slothyfred <alfredmbinayan@gmail.com>
2024-04-16 10:17:41 +0800

    First commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/books', [BookController::class, 'index']);
+
 
 Route::get('/', function () {
     return view('welcome');
